4b62232b2824c7f70b06b4d2c303dff0ee5bd156
qijianhong <qijianhong@yunlizhihui.com>
2019-10-11 17:31:46 +0800

    增加redis缓存

--- spatial-query/src/main/java/com/wyb/spatialquery/service/impl/PointOfInterestServiceImpl.java
+++ spatial-query/src/main/java/com/wyb/spatialquery/service/impl/PointOfInterestServiceImpl.java
@@ -1,19 +1,19 @@
 package com.wyb.spatialquery.service.impl;
 
-import com.wyb.spatialquery.domain.PointOfInterest;
 import com.wyb.spatialquery.dao.PointOfInterestMapper;
+import com.wyb.spatialquery.domain.PointOfInterest;
 import com.wyb.spatialquery.service.PointOfInterestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * POI相关
+ *
  * @author wangyongbing
- * @date 2019/9/8 19:59
  * @version 1.0.0
+ * @date 2019/9/8 19:59
  */
 @Service
 public class PointOfInterestServiceImpl implements PointOfInterestService {
@@ -27,7 +27,6 @@ public class PointOfInterestServiceImpl implements PointOfInterestService {
 
     @Override
     public List<PointOfInterest> queryByExtent(String extent) {
-        System.out.println("第一次执行");
         return this.pointOfInterestMapper.findByExtent(extent);
     }
 
