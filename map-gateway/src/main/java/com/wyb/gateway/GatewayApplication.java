314cb8e449d2e4c8a8f6cdb56a161c99259fac90
wyblzu <1026809921@qq.com>
2019-10-17 23:26:46 +0800

    增加网关

---
+++ map-gateway/src/main/java/com/wyb/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.wyb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * 网关
+ *
+ * @author wangyongbing
+ * @version 1.0.0
+ * @date 2019/10/15 22:31
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
