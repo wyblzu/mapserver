0092a20f4acc15034ca878399aa394e313d9506d
wyblzu <1026809921@qq.com>
2020-01-05 00:32:36 +0800

    网关添加跨域

---
+++ map-gateway/src/main/java/com/wyb/gateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.wyb.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * 网关跨域
+ *
+ * @author wangyongbing
+ * @version 1.0.0
+ * @since 2020-01-05 上午12:27
+ **/
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
